05c949dc1597720957907c2fe043461ffc1d7604
Francisco <francisco.valerio@enlightware.ch>
2024-07-16 10:24:19 +0200

    Cleaned up code editor component.
    Former-commit-id: 832f78af066a82e058c5cb02128a966a4c2b598c

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -3,8 +3,7 @@
 import { ref, onMounted, type Ref } from 'vue';
 import { Compiler } from 'script-api';
 
-// import { javascript } from '@codemirror/lang-javascript';
-import { EditorView } from '@codemirror/view';
+import { EditorView, ViewUpdate } from '@codemirror/view';
 import { basicSetup } from 'codemirror';
 import { nonnull } from '../types';
 import { renderErrorDataPlugin, setErrorUnderlines } from '../error-underline-extension';
@@ -13,33 +12,33 @@ import { renderAnnotationsPlugin, setAnnotations } from '../annotation-extension
 const editor: Ref<null | HTMLElement> = ref(null);
 const compiler = new Compiler();
 
+function processUpdate(update: ViewUpdate){
+	const text = update.state.doc.toString();
+	const view = update.view;
+	// const newText = processText(text);
+	// if (newText !== text) {
+	// 	view.dispatch({changes: {from: 0, to: newText.length, insert: newText}});
+	// }
+	if (update.docChanged) {
+		const errorData = compiler.compile(text);
+		if (errorData !== undefined) {
+			setErrorUnderlines(view, errorData);
+			setAnnotations(view, []);
+		} else {
+			setErrorUnderlines(view, []);
+			setAnnotations(view, compiler.get_annotations());
+		}
+	}
+}
+
 onMounted(() => {
-	const viewPtr: unknown[] = [null];
-	viewPtr[0] = new EditorView({
+	new EditorView({
 		doc: "",
 		extensions: [
 			basicSetup,
-			// javascript(),
 			renderErrorDataPlugin,
 			renderAnnotationsPlugin,
-			EditorView.updateListener.of((update) => {
-				const text = update.state.doc.toString();
-				const view = (viewPtr[0] as EditorView);
-				// const newText = processText(text);
-				// if (newText !== text) {
-				// 	view.dispatch({changes: {from: 0, to: newText.length, insert: newText}});
-				// }
-				if (update.docChanged) {
-					const errorData = compiler.compile(text);
-					if (errorData !== undefined) {
-						setErrorUnderlines(view, errorData);
-						setAnnotations(view, []);
-					} else {
-						setErrorUnderlines(view, []);
-						setAnnotations(view, compiler.get_annotations());
-					}
-				}
-			})
+			EditorView.updateListener.of(processUpdate)
 		],
 		parent: nonnull(editor.value),
 	});
