183f5f4c8b0307d7163cd2c13af644bbe96e2d16
Francisco <francisco.valerio@enlightware.ch>
2024-07-18 12:14:26 +0200

    Refactor grammar and improve syntax highlighting
    Former-commit-id: 1a7c706597336450d78f983405747b2b8c3c45c4

--- src/language/language-extension.ts
+++ src/language/language-extension.ts
@@ -1,25 +1,35 @@
 import { parser } from "./language.grammar";
-import { LRLanguage, LanguageSupport } from "@codemirror/language";
+import { LRLanguage, LanguageSupport, continuedIndent, indentNodeProp } from "@codemirror/language";
 import { styleTags, tags as t } from "@lezer/highlight";
+
+// list if tags: https://lezer.codemirror.net/docs/ref/#highlight.tags
+const highlight = styleTags({
+	"fn let var": t.definitionKeyword,
+	"if else match return": t.controlKeyword,
+	Identifier: t.variableName,
+	Integer: t.integer,
+	BoolLiteral: t.bool,
+	String: t.string,
+	LineComment: t.comment,
+	ArithOp: t.arithmeticOperator,
+	LogicOp: t.logicOperator,
+	CompareOp: t.compareOperator,
+	"( )": t.paren,
+	"[ ]": t.squareBracket,
+	"{ }": t.brace,
+	", : ; ::": t.separator,
+	"=": t.definitionOperator,
+	"=>": t.punctuation,
+})
 
 export function languageExtension() {
 	return new LanguageSupport(LRLanguage.define({
 		name: "Candli script",
 		parser: parser.configure({
 			props: [
-				styleTags({
-					Identifier: t.variableName,
-					Integer: t.integer,
-					BoolLiteral: t.bool,
-					String: t.string,
-					LineComment: t.comment,
-					fn: t.definitionKeyword,
-					let: t.definitionKeyword,
-					var: t.definitionKeyword,
-					return: t.keyword,
-					if: t.controlKeyword,
-					else: t.controlKeyword,
-					"( )": t.paren
+				highlight,
+				indentNodeProp.add({
+					"Expr Function": continuedIndent(),
 				}),
 			]
 		}),
