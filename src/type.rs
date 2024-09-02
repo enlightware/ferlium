7d254ba3e2a33c9a495a0c8ad3c3f354b1fd2499
Stéphane Magnenat <stephane.magnenat@enlightware.ch>
2024-09-02 15:43:19 +0200

    Removed old subtyping detection code as it was not compatible with current HM-type inference.

--- src/type.rs
+++ src/type.rs
@@ -212,16 +212,6 @@ impl FnArgType {
             .local_cmp(&other.ty)
             .then(self.inout.cmp(&other.inout))
     }
-    fn can_be_used_in_place_of_with_subst(
-        self,
-        other: Self,
-        substitutions: &mut TypeSubstitution,
-        seen: &mut HashSet<(Type, Type)>,
-    ) -> bool {
-        self.ty
-            .can_be_used_in_place_of_with_subst(other.ty, substitutions, seen)
-            && (self.inout.is_mutable() || !other.inout.is_mutable())
-    }
 }
 impl FmtWithModuleEnv for FnArgType {
     fn fmt_with_module_env(&self, f: &mut fmt::Formatter, env: &ModuleEnv<'_>) -> fmt::Result {
@@ -457,51 +447,6 @@ impl Type {
     // filter
     pub fn is_local(self) -> bool {
         self.world.is_none()
-    }
-
-    // subtyping
-    pub fn can_be_used_in_place_of_with_subst(
-        self,
-        other: Self,
-        substitutions: &mut TypeSubstitution,
-        seen: &mut HashSet<(Type, Type)>,
-    ) -> bool {
-        // If the types are the same, they can be used in place of each other
-        if self == other {
-            return true;
-        }
-        // If we are recursing, prevent subtyping cycles
-        // TODO: do smarter matching to support recursive types
-        if seen.contains(&(self, other)) {
-            return false;
-        }
-        // A generic ref can be replaced by anything, but keep in mind the substitutions
-        if let TypeKind::Variable(that_index) = *other.data() {
-            match substitutions.get(&that_index) {
-                Some(subst) => {
-                    seen.insert((self, other));
-                    return self.can_be_used_in_place_of_with_subst(*subst, substitutions, seen);
-                }
-                None => {
-                    // do not perform substitution if we already have the correct index
-                    if let TypeKind::Variable(this_index) = *self.data() {
-                        if this_index == that_index {
-                            return true;
-                        }
-                    }
-                    substitutions.insert(that_index, self);
-                    return true;
-                }
-            }
-        }
-        // Otherwise, we need to do a structural comparison
-        seen.insert((self, other));
-        self.data()
-            .can_be_used_in_place_of_with_subst(other, substitutions, seen)
-    }
-
-    pub fn can_be_used_in_place_of(self, that: Self) -> bool {
-        self.can_be_used_in_place_of_with_subst(that, &mut HashMap::new(), &mut HashSet::new())
     }
 
     // other
@@ -830,126 +775,6 @@ impl TypeKind {
 
         // process self
         visitor.visit_end(self);
-    }
-
-    /// Somewhat a sub-type, but named differently to accommodate generics
-    fn can_be_used_in_place_of_with_subst(
-        &self,
-        other: Type,
-        substitutions: &mut TypeSubstitution,
-        seen: &mut HashSet<(Type, Type)>,
-    ) -> bool {
-        // We know that that is not a GenericArg
-        match self {
-            // A generic type can be used in place of itself with compatible type arguments, or instantiate a generics
-            TypeKind::Native(this_gen) => match &*other.data() {
-                TypeKind::Native(that_gen) => {
-                    this_gen.bare_ty == that_gen.bare_ty
-                        && this_gen.arguments.len() == that_gen.arguments.len()
-                        && this_gen // covariant argument types
-                            .arguments
-                            .iter()
-                            .zip(that_gen.arguments.iter())
-                            .all(|(this_ty, that_ty)| {
-                                this_ty.can_be_used_in_place_of_with_subst(
-                                    *that_ty,
-                                    substitutions,
-                                    seen,
-                                )
-                            })
-                }
-                _ => false,
-            },
-            // We trait generics as invariant
-            TypeKind::Variable(_) => false,
-            // This variant can be used in place of that variant if for every constructor and argument in that variant,
-            // there is a constructor and argument in this union that can be used in place of it.
-            TypeKind::Variant(this_variant) => match &*other.data() {
-                TypeKind::Variant(that_variant) => that_variant.iter().all(|that_ctor| {
-                    this_variant.iter().any(|this_ctor| {
-                        this_ctor.0 == that_ctor.0
-                            && this_ctor.1.can_be_used_in_place_of_with_subst(
-                                that_ctor.1,
-                                substitutions,
-                                seen,
-                            )
-                    })
-                }),
-                _ => false,
-            },
-            // Larger tuples can be used in place of smaller tuples
-            TypeKind::Tuple(this_tuple) => match &*other.data() {
-                TypeKind::Tuple(that_tuple) => {
-                    this_tuple.len() >= that_tuple.len()
-                        && this_tuple // covariant element types
-                            .iter()
-                            .zip(that_tuple.iter())
-                            .all(|(this_ty, that_ty)| {
-                                this_ty.can_be_used_in_place_of_with_subst(
-                                    *that_ty,
-                                    substitutions,
-                                    seen,
-                                )
-                            })
-                }
-                _ => false,
-            },
-            // This record can be used in place of that record if for every field in that record,
-            // there is a field in this record that can be used in place of it.
-            // FIXME: this currently will generate wrong code because of the order of fields
-            TypeKind::Record(this_record) => match &*other.data() {
-                TypeKind::Record(that_record) => that_record.iter().all(|(that_name, that_ty)| {
-                    this_record.iter().any(|(this_name, this_ty)| {
-                        this_name == that_name
-                            && this_ty.can_be_used_in_place_of_with_subst(
-                                *that_ty,
-                                substitutions,
-                                seen,
-                            )
-                    })
-                }),
-                _ => false,
-            },
-            // A function can be used in place of another function if the argument types are contravariant and return type covariant.
-            TypeKind::Function(this_fn) => {
-                let this_args = &this_fn.args;
-                let this_ty = &this_fn.ret;
-                match &*other.data() {
-                    TypeKind::Function(that_fun) => {
-                        let that_args = &that_fun.args;
-                        let that_ty = &that_fun.ret;
-                        this_args.len() == that_args.len()
-                            && this_args // contravariant argument types
-                                .iter()
-                                .zip(that_args.iter())
-                                .all(|(this_ty, that_ty)| {
-                                    that_ty.can_be_used_in_place_of_with_subst(
-                                        *this_ty,
-                                        substitutions,
-                                        seen,
-                                    )
-                                })
-                            && this_ty.can_be_used_in_place_of_with_subst(
-                                *that_ty,
-                                substitutions,
-                                seen,
-                            )
-                        // covariant return type
-                    }
-                    _ => false,
-                }
-            }
-            // A new type can be used in place of the type it wraps
-            TypeKind::Newtype(_, this_ty) => {
-                this_ty.can_be_used_in_place_of_with_subst(other, substitutions, seen)
-            }
-            // The never type cannot be used in place of any type
-            TypeKind::Never => false,
-        }
-    }
-
-    pub fn can_be_used_in_place_of(&self, that: Type) -> bool {
-        self.can_be_used_in_place_of_with_subst(that, &mut HashMap::new(), &mut HashSet::new())
     }
 
     /// Returns whether the type contains the given type variable
@@ -1367,16 +1192,8 @@ mod tests {
         let _string = Type::primitive::<String>();
         let _gen_arg0 = Type::variable_id(0);
 
-        // Primitive types
-        assert!(_i32.can_be_used_in_place_of(_i32));
-        assert!(_i32.can_be_used_in_place_of(_gen_arg0));
-        assert!(!_i32.can_be_used_in_place_of(_f32));
-        assert!(!_i32.can_be_used_in_place_of(_string));
-
         // Generics
-        assert!(_gen_arg0.can_be_used_in_place_of(_gen_arg0));
         let _gen_arg1 = Type::variable_id(1);
-        assert!(_gen_arg0.can_be_used_in_place_of(_gen_arg1));
         #[derive(Debug, Clone)]
         struct List;
         let _gen_unbound = Type::native::<List>(vec![_gen_arg0]);
@@ -1390,32 +1207,6 @@ mod tests {
         let _gen_2_partial_bound_a_i32 = Type::native::<Map>(vec![_gen_arg0, _i32]);
         let _gen_2_bound_i32_i32 = Type::native::<Map>(vec![_i32, _i32]);
         let _gen_2_bound_i32_f32 = Type::native::<Map>(vec![_i32, _f32]);
-        assert!(_gen_unbound.can_be_used_in_place_of(_gen_unbound));
-        assert!(_gen_bound_i32.can_be_used_in_place_of(_gen_unbound));
-        assert!(_gen_bound_string.can_be_used_in_place_of(_gen_unbound));
-        assert!(_gen_2_unbound_a_b.can_be_used_in_place_of(_gen_2_unbound_a_b));
-        assert!(_gen_2_unbound_b_a.can_be_used_in_place_of(_gen_2_unbound_b_a));
-        assert!(_gen_2_unbound_a_b.can_be_used_in_place_of(_gen_2_unbound_b_a));
-        assert!(_gen_2_unbound_b_a.can_be_used_in_place_of(_gen_2_unbound_a_b));
-        assert!(_gen_2_partial_bound_i32_a.can_be_used_in_place_of(_gen_2_partial_bound_i32_a));
-        assert!(_gen_2_partial_bound_a_i32.can_be_used_in_place_of(_gen_2_partial_bound_a_i32));
-        assert!(!_gen_2_partial_bound_i32_a.can_be_used_in_place_of(_gen_2_partial_bound_a_i32));
-        assert!(_gen_2_partial_bound_i32_a.can_be_used_in_place_of(_gen_2_unbound_a_b));
-        assert!(_gen_2_partial_bound_i32_a.can_be_used_in_place_of(_gen_2_unbound_b_a));
-        assert!(_gen_2_partial_bound_a_i32.can_be_used_in_place_of(_gen_2_unbound_a_b));
-        assert!(_gen_2_partial_bound_a_i32.can_be_used_in_place_of(_gen_2_unbound_b_a));
-        assert!(_gen_2_bound_i32_i32.can_be_used_in_place_of(_gen_2_unbound_a_b));
-        assert!(_gen_2_bound_i32_i32.can_be_used_in_place_of(_gen_2_unbound_b_a));
-        assert!(_gen_2_bound_i32_i32.can_be_used_in_place_of(_gen_2_partial_bound_i32_a));
-        assert!(_gen_2_bound_i32_i32.can_be_used_in_place_of(_gen_2_partial_bound_a_i32));
-        assert!(_gen_2_bound_i32_i32.can_be_used_in_place_of(_gen_2_bound_i32_i32));
-        assert!(!_gen_2_bound_i32_i32.can_be_used_in_place_of(_gen_2_bound_i32_f32));
-        assert!(_gen_2_bound_i32_f32.can_be_used_in_place_of(_gen_2_unbound_a_b));
-        assert!(_gen_2_bound_i32_f32.can_be_used_in_place_of(_gen_2_unbound_b_a));
-        assert!(_gen_2_bound_i32_f32.can_be_used_in_place_of(_gen_2_partial_bound_i32_a));
-        assert!(!_gen_2_bound_i32_f32.can_be_used_in_place_of(_gen_2_partial_bound_a_i32));
-        assert!(!_gen_2_bound_i32_f32.can_be_used_in_place_of(_gen_2_bound_i32_i32));
-        assert!(_gen_2_bound_i32_f32.can_be_used_in_place_of(_gen_2_bound_i32_f32));
 
         // Variants
         let _i_s = ustr("i");
@@ -1427,14 +1218,6 @@ mod tests {
             Type::variant(vec![(_i_s, _i32), (_f_s, _f32), (_s_s, _string)]);
         let _union_string_i32_f32 =
             Type::variant(vec![(_s_s, _string), (_i_s, _i32), (_f_s, _f32)]);
-        assert!(_variant_i32_f32.can_be_used_in_place_of(_variant_i32_f32));
-        assert!(_variant_f32_i32.can_be_used_in_place_of(_variant_f32_i32));
-        assert!(_variant_i32_f32.can_be_used_in_place_of(_variant_f32_i32));
-        assert!(_variant_f32_i32.can_be_used_in_place_of(_variant_i32_f32));
-        assert!(_variant_i32_f32_string.can_be_used_in_place_of(_variant_i32_f32));
-        assert!(_variant_i32_f32_string.can_be_used_in_place_of(_variant_f32_i32));
-        assert!(_union_string_i32_f32.can_be_used_in_place_of(_variant_i32_f32));
-        assert!(_union_string_i32_f32.can_be_used_in_place_of(_variant_f32_i32));
 
         // Recursive variants
         let empty_tuple = Type::tuple(vec![]);
@@ -1443,19 +1226,12 @@ mod tests {
             (ustr("Nil"), empty_tuple),
             (ustr("Cons"), Type::new_local(0)),
         ]);
-        let [adt_gen_list_element, adt_gen_list] =
+        let [_adt_gen_list_element, _adt_gen_list] =
             store_types(&[adt_gen_list_element_td, adt_list_td.clone()]);
-        assert!(adt_gen_list.can_be_used_in_place_of(adt_gen_list));
-        assert!(adt_gen_list_element.can_be_used_in_place_of(adt_gen_list_element));
-        assert!(!adt_gen_list.can_be_used_in_place_of(adt_gen_list_element));
-        assert!(!adt_gen_list_element.can_be_used_in_place_of(adt_gen_list));
-        assert!(adt_gen_list.can_be_used_in_place_of(_gen_arg1));
         // FIXME: support recursive types
         // TODO: read Subtyping Recursive Types, Luca Cardelli, 1993
         // let adt_int_list_element_td = TypeData::Tuple(vec![_i32, Type::new_local(1)]);
         // let [adt_int_list_element, adt_int_list] = store_types(&[adt_int_list_element_td, adt_list_td]);
-        // assert!(adt_int_list.can_be_used_in_place_of(adt_gen_list));
-        // assert!(adt_int_list_element.can_be_used_in_place_of(adt_gen_list_element));
 
         // Tuples
         let _tuple_i32 = Type::tuple(vec![_i32]);
@@ -1463,18 +1239,10 @@ mod tests {
         let _tuple_i32_i32 = Type::tuple(vec![_i32, _i32]);
         let _tuple_i32_f32 = Type::tuple(vec![_i32, _f32]);
         let _tuple_f32_i32 = Type::tuple(vec![_f32, _i32]);
-        assert!(_tuple_i32.can_be_used_in_place_of(_tuple_i32));
-        assert!(!_tuple_i32.can_be_used_in_place_of(_tuple_f32));
-        assert!(_tuple_i32_i32.can_be_used_in_place_of(_tuple_i32));
-        assert!(_tuple_i32_f32.can_be_used_in_place_of(_tuple_i32));
-        assert!(!_tuple_f32_i32.can_be_used_in_place_of(_tuple_i32));
         let _tuple_gen0_gen1 = Type::tuple(vec![_gen_arg0, _gen_arg1]);
-        assert!(_tuple_i32_i32.can_be_used_in_place_of(_tuple_gen0_gen1));
         let _tuple_i32_tuple_f32_i32 = Type::tuple(vec![_i32, _tuple_f32_i32]);
-        assert!(_tuple_i32_tuple_f32_i32.can_be_used_in_place_of(_tuple_gen0_gen1));
         let _gen_arg2 = Type::variable_id(2);
         let _tuple_i32_tuple_f32_gen2 = Type::tuple(vec![_i32, Type::tuple(vec![_f32, _gen_arg2])]);
-        assert!(_tuple_i32_tuple_f32_i32.can_be_used_in_place_of(_tuple_i32_tuple_f32_gen2));
 
         // Record
         let x = ustr("x");
@@ -1486,20 +1254,6 @@ mod tests {
         let _record_vec3_f32_shuffled = Type::record(vec![(z, _f32), (x, _f32), (y, _f32)]);
         let _record_vec2_gen = Type::record(vec![(x, _gen_arg0), (y, _gen_arg0)]);
         let _record_het = Type::record(vec![(x, _i32), (y, _f32)]);
-        assert!(_record_vec2_i32.can_be_used_in_place_of(_record_vec2_i32));
-        assert!(_record_vec2_f32.can_be_used_in_place_of(_record_vec2_f32));
-        assert!(!_record_vec2_i32.can_be_used_in_place_of(_record_vec2_f32));
-        assert!(_record_vec3_f32.can_be_used_in_place_of(_record_vec2_f32));
-        assert!(!_record_vec2_f32.can_be_used_in_place_of(_record_vec3_f32));
-        assert!(_record_vec3_f32_shuffled.can_be_used_in_place_of(_record_vec3_f32));
-        assert!(_record_vec3_f32.can_be_used_in_place_of(_record_vec3_f32_shuffled));
-        assert!(_record_vec3_f32_shuffled.can_be_used_in_place_of(_record_vec2_f32));
-        assert!(_record_vec2_i32.can_be_used_in_place_of(_record_vec2_gen));
-        assert!(!_record_vec2_gen.can_be_used_in_place_of(_record_vec2_i32));
-        assert!(_record_het.can_be_used_in_place_of(_record_het));
-        assert!(!_record_vec2_i32.can_be_used_in_place_of(_record_het));
-        assert!(!_record_het.can_be_used_in_place_of(_record_vec2_i32));
-        assert!(!_record_het.can_be_used_in_place_of(_record_vec2_gen));
 
         // Native functions
         // unary functions
@@ -1509,28 +1263,12 @@ mod tests {
         let _native_fn_f32_f32 = Type::unary_function_by_val(_f32, _f32);
         let _native_fn_any_i32 = Type::unary_function_by_val(_gen_arg0, _i32);
         let _native_fn_i32_any = Type::unary_function_by_val(_i32, _gen_arg0);
-        assert!(_native_fn_i32_i32.can_be_used_in_place_of(_native_fn_i32_i32));
-        assert!(_native_fn_i32_f32.can_be_used_in_place_of(_native_fn_i32_f32));
-        assert!(!_native_fn_i32_i32.can_be_used_in_place_of(_native_fn_i32_f32));
-        assert!(_native_fn_any_i32.can_be_used_in_place_of(_native_fn_i32_i32));
-        assert!(_native_fn_any_i32.can_be_used_in_place_of(_native_fn_f32_i32));
-        assert!(!_native_fn_any_i32.can_be_used_in_place_of(_native_fn_i32_f32));
-        assert!(!_native_fn_any_i32.can_be_used_in_place_of(_native_fn_f32_f32));
-        assert!(!_native_fn_i32_any.can_be_used_in_place_of(_native_fn_i32_i32));
-        assert!(_native_fn_i32_i32.can_be_used_in_place_of(_native_fn_i32_any));
 
         // binary functions
         // TODO: add more tests
 
         // new types
         let _int = Type::new_type(ustr("Int"), _i32);
-        assert!(_int.can_be_used_in_place_of(_int));
-        assert!(_int.can_be_used_in_place_of(_gen_arg0));
-        assert!(_int.can_be_used_in_place_of(_i32));
         let _other_int = Type::new_type(ustr("OtherInt"), _i32);
-        assert!(_other_int.can_be_used_in_place_of(_other_int));
-        assert!(_other_int.can_be_used_in_place_of(_gen_arg0));
-        assert!(_other_int.can_be_used_in_place_of(_i32));
-        assert!(!_other_int.can_be_used_in_place_of(_int));
     }
 }
