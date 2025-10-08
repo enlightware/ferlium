a99c3d3865ffe6a063d714a5c55751e482908560
Stéphane Magnenat <stephane.magnenat@enlightware.ch>
2025-10-08 15:47:57 +0200

    Fixed location of use directive wrt license header.

--- src/value.rs
+++ src/value.rs
@@ -1,4 +1,3 @@
-use derive_new::new;
 // Copyright 2025 Enlightware GmbH
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
@@ -7,6 +6,7 @@ use derive_new::new;
 //
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 //
+use derive_new::new;
 use dyn_clone::DynClone;
 use dyn_eq::DynEq;
 use dyn_hash::DynHash;
