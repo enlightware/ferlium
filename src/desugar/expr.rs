ff609cff71e0bad608a88fafa0f9d10ac31998c8
Stéphane Magnenat <stephane.magnenat@enlightware.ch>
2026-08-04 12:08:09 +0200

    Added missing license headers

--- src/desugar/expr.rs
+++ src/desugar/expr.rs
@@ -1,3 +1,11 @@
+// Copyright 2026 Enlightware GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+//
 use super::format_string::emit_format_string_ast;
 use super::patterns::{desugar_block_exprs, desugar_let_exprs, desugar_pattern_bindings};
 use super::*;
